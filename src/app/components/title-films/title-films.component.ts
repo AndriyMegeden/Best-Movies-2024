00c648d7f5124bb8a38277716e5d5c128b8d5232
Andiy <andriemeheden@gmail.com>
2025-01-02 18:34:35 +0200

    fix and change title

--- src/app/components/title-films/title-films.component.ts
+++ src/app/components/title-films/title-films.component.ts
@@ -19,56 +19,55 @@ export class TitleFilmsComponent {
     //   smooth: 1.5,
     //   effects: true
     // });
-    if (ScrollTrigger.isTouch !== 1) {
-      gsap.fromTo(
-        '.main-container',
-        { opacity: 1 },
-        {
-          duration: 2.5,
-          opacity: 0,
-          scrollTrigger: {
-            trigger: '.main-container',
-            markers: false,
-            start: '200 top',
-            end: 'bottom',
-            scrub: true,
-          },
-        }
-      );
-      gsap.from('.best-title', { opacity: 0, duration: 1, y: -50 });
-      gsap.fromTo(
-        '.best-title',
-        {},
-        {
-          y: 50,
-          ease: 'none',
-          duration: 3,
-          scrollTrigger: {
-            trigger: '.title',
-            markers: false,
-            start: '-220 top',
-            end: '-50 40',
-            scrub: 1,
-          },
-        }
-      );
-      // gsap.from('.cards', { opacity: 0, duration: 1, y: -50 });
-      // gsap.fromTo(
-      //   '.cards',
-      //   {},
-      //   {
-      //     y: 50,
-      //     ease: 'none',
-      //     duration: 2.5,
-      //     scrollTrigger: {
-      //       trigger: '.cards',
-      //       start: '-100 top',
-      //       end: '800, center',
-      //       markers: false,
-      //       scrub: true,
-      //     },
-      //   }
-      // );
-    }
+
+    gsap.fromTo(
+      '.main-container',
+      { opacity: 1 },
+      {
+        duration: 2.5,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: '.main-container',
+          markers: false,
+          start: '200 top',
+          end: 'bottom',
+          scrub: true,
+        },
+      }
+    );
+    gsap.from('.best-title', { opacity: 0, duration: 1, y: -50 });
+    gsap.fromTo(
+      '.best-title',
+      {},
+      {
+        y: 50,
+        ease: 'none',
+        duration: 3,
+        scrollTrigger: {
+          trigger: '.title',
+          markers: false,
+          start: '-220 top',
+          end: '-50 40',
+          scrub: 1,
+        },
+      }
+    );
+    gsap.from('.cards', { opacity: 0, duration: 1, y: -50 });
+    // gsap.fromTo(
+    //   '.cards',
+    //   {},
+    //   {
+    //     y: 50,
+    //     ease: 'none',
+    //     duration: 2.5,
+    //     scrollTrigger: {
+    //       trigger: '.cards',
+    //       start: '-100 top',
+    //       end: '800, center',
+    //       markers: false,
+    //       scrub: true,
+    //     },
+    //   }
+    // );
   }
 }
